bd508803d8f5f4e2fa507764a15d343c7774d472
arun980798 <arunrajput88176894@gmail.com>
2025-11-05 23:23:25 +0530

    q8 done

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-// 7. Ask user’s age and check if eligible to vote
-// If age >= 18 → “Eligible”, else → “Not eligible”
-
-let age = +prompt("give your age");
-console.log(age)
 
 
+// 8. Print multiplication table of 5
+// Use loop to print 5 × 1 to 5 × 10.
 
-if (age === null || age === undefined) {
-  console.error("give the output")
-}
-else if (isNaN(age)) {
-  console.error("give the age");
-}
-else {
-  if (age < 18) {
-    console.log(" you cant vote ")
-  }
-  else {
-    console.log("now you can vote")
-  }
-
+for (let i = 1; i <= 10; i++) {
+  console.log(i * 5);
 }
