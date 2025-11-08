99c8bf7713caa878da263a23641e6da94ee6aa57
arun980798 <arunrajput88176894@gmail.com>
2025-11-08 19:04:05 +0530

    q13 done

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
 
+// 13. Print numbers divisible by 7 from 1 to 50
+// Use modulo % and loop.
 
 
-
-// 12. Ask user for words until they type “stop”. Count how many times they typed “yes”
-// Loop until "stop" is typed. Count "yes".
-
-
-let con = false;
-let count = 0
-let word = prompt("give your words ");
-con = word.includes("stop")
-count++
-
-while (!con) {
-  word = prompt("give your words ");
-  count++
-  con = word.includes("stop");
-  if (con) {
-    break;
+for (let i = 0; i <= 50; i++) {
+  if (i % 7 === 0) {
+    console.log(i)
   }
 }
-console.log(count)
