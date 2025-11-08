6afb31c9189a47c8641cc92774d6479b5a1f361b
arun980798 <arunrajput88176894@gmail.com>
2025-11-08 14:09:56 +0530

    q12 done

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 
 
-// 11. Allow only 3 attempts to enter correct password
-// If user gets it right early, stop. If not → “Account locked”
 
 
-const pass = "arun";
+// 12. Ask user for words until they type “stop”. Count how many times they typed “yes”
+// Loop until "stop" is typed. Count "yes".
 
-let count = 0;
 
-let currentPass = prompt("Enter the password:");
-++count;
+let con = false;
+let count = 0
+let word = prompt("give your words ");
+con = word.includes("stop")
+count++
 
-if (currentPass === pass) {
-  console.log("Welcome");
-} else {
-  for (let i = count; i < 3; i++) {
-    currentPass = prompt("Enter the password:");
-    ++count;
-
-    if (currentPass === pass) {
-      console.log("Welcome");
-      break;
-    }
+while (!con) {
+  word = prompt("give your words ");
+  count++
+  con = word.includes("stop");
+  if (con) {
+    break;
   }
 }
+console.log(count)
